3dcaa7fa2c43cf33517bfc786d74d50f92a37e2e
Ticiana De Lello <ticiana@poli.ufrj.br>
2016-11-13 16:27:34 -0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'po(gyr21212c8x4=pz^)39+oze37%83ueh@qjl&ri6bd052o%t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
